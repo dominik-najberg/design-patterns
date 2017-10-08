52032ea4a552e4bd613095d8b886b7bdc1bd7972
Dominik Najberg <dominik.najberg@gmail.com>
2017-10-08 21:15:09 +0200

    Factory Pattern - initial version

---
+++ src/FactoryBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FactoryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('FactoryBundle:Default:index.html.twig');
+    }
+}
