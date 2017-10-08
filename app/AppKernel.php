a27b0cf7658f7fa931d6380fc5c0296a265f7482
Dominik Najberg <dominik.najberg@gmail.com>
2017-10-08 15:37:03 +0200

    Are you ready to order some pizza from a Factory?

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new ObserverPatternBundle\ObserverPatternBundle(),
             new DuckBundle\DuckBundle(),
             new DecoratorBundle\DecoratorBundle(),
+            new FactoryBundle\FactoryBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
