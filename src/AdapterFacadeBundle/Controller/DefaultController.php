ca9388dfb5048a7904bab07dfed29557d680d21a
Dominik Najberg <dominik.najberg@gmail.com>
2017-10-17 14:30:29 +0200

    How to use services in Symfony 3.3

--- src/AdapterFacadeBundle/Controller/DefaultController.php
+++ src/AdapterFacadeBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AdapterFacadeBundle\Controller;
 
+use AdapterFacadeBundle\Adapters\DuckAdapter;
 use AdapterFacadeBundle\Adapters\TurkeyAdapter;
 use AdapterFacadeBundle\Birds\MallardDuck;
 use AdapterFacadeBundle\Birds\WildTurkey;
+use AppBundle\Service\MadService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -18,10 +20,15 @@ class DefaultController extends Controller
         $duck = new MallardDuck();
         $turkey = new WildTurkey();
         $phonyDuck = new TurkeyAdapter($turkey);
+        $phonyTurkey = new DuckAdapter($duck);
+        $serviceTest = $this->get(MadService::class)->getSomethingInteresting();
 
         $vars = ['duck' => $duck,
             'turkey' => $turkey,
-            'phonyDuck' => $phonyDuck];
+            'phonyDuck' => $phonyDuck,
+            'phonyTurkey' => $phonyTurkey,
+            'serviceTest' => $serviceTest,
+        ];
 
         return $this->render('AdapterFacadeBundle:Default:index.html.twig', $vars);
     }
